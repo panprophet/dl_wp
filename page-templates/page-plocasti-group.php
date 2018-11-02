398a4ea724dff9fda2f53711e3d4cd7a506da015
ycas2002 <ycas2002@yahoo.com>
2018-11-02 14:45:53 +0100

    plocasti

---
+++ page-templates/page-plocasti-group.php
@@ -0,0 +1,15 @@
+<?php /* Template Name: Plocasti materijali */ ?>
+
+<?php
+  global $post;
+  $post_slug = $post->post_name;
+  echo $post_slug;
+ ?>
+<div class="materijal">
+  <div class="materijal--container" style="background-image: url(); background-size: cover; background-repeat: no-repeat; background-position: center;">
+    <div class="materijal--container-text">
+      <div class="materijal--container-text--title"></div>
+      <div class="materijal--container-text--paragraph"></div>
+    </div>
+  </div>
+</div>
