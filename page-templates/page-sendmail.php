367a4200e61a63f3e19a0cc19c39d0bd9782a351
ycas2002 <ycas2002@yahoo.com>
2018-11-08 15:46:27 +0100

    php mail

--- page-templates/page-sendmail.php
+++ page-templates/page-sendmail.php
@@ -7,7 +7,7 @@
     $subject = "Kontakt";
     $message = $_POST["message"];
     $message = 'Poruka od: '.$imeprezime.'\r\n'.$message;
-    $headers = "From: <$from>";
+    $headers = "From: $imeprezime <$from>";
 
     if (mail($to, $subject, $message, $headers)) {
         echo  "<div class=\"response\"><div>Poruka uspešno poslata!</div><div>Javićemo vam se ubrzo.</div></div>";
