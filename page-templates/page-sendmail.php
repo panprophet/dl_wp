8d2f801ca2d9b256f8d7c0c934f06549493e2c1d
ycas2002 <ycas2002@yahoo.com>
2018-11-08 17:11:56 +0100

    sand mail

--- page-templates/page-sendmail.php
+++ page-templates/page-sendmail.php
@@ -6,18 +6,21 @@
     $to = "office@drvolux.rs";
     $from = $_POST["mail"];
     $imeprezime = $_POST["imeprezime"];
-    // pitaj za subject
     $subject = $_POST["naslov"];
     $message = $_POST["message"];
     $message = 'Poruka od: '.$imeprezime.'\r\n'.$message;
     $headers = "From: <$from>"."\r\n"."Reply-to: <$from>"."\r\n".'X-Mailer: PHP/'.phpversion();
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo  "<div class=\"response--message\"><div>Poruka uspešno poslata!</div><div>Javićemo vam se ubrzo.</div></div>";
+    // if (mail($to, $subject, $message, $headers)) {
+    //     echo  "<div class=\"response--message\"><div>Poruka uspešno poslata!</div><div>Javićemo vam se ubrzo.</div></div>";
+    // } else {
+    //     echo "<div class=\"response--message\"><div> NOT OK </div></div>";
+    // }
+    if(wp_mail($to, $subject, $message, $headers)) {
+        wp_redirect(get_permalink(get_page_by_title('Kontakt')));
     } else {
-        echo "<div class=\"response--message\"><div> NOT OK </div></div>";
+        echo "ne valja";
     }
-
 ?>
 </div>
 <?php get_footer(); ?>
