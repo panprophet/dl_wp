5677aa937a54da6d2c07863fb67effd8186e7a03
ycas2002 <ycas2002@yahoo.com>
2018-11-08 17:14:03 +0100

    send mail

--- page-templates/page-sendmail.php
+++ page-templates/page-sendmail.php
@@ -1,18 +1,26 @@
-  <?php get_header(); ?>
+<?php /* Template Name: Sent mail */ ?>
+
+<?php get_header(); ?>
+<div class="response">
   <?php
     $to = "office@drvolux.rs";
     $from = $_POST["mail"];
     $imeprezime = $_POST["imeprezime"];
-    // pitaj za subject
-    $subject = "Kontakt";
+    $subject = $_POST["naslov"];
     $message = $_POST["message"];
     $message = 'Poruka od: '.$imeprezime.'\r\n'.$message;
-    $headers = "From: $imeprezime <$from>";
+    $headers = "From: <$from>"."\r\n"."Reply-to: <$from>"."\r\n".'X-Mailer: PHP/'.phpversion();
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo  "<div class=\"response\"><div>Poruka uspešno poslata!</div><div>Javićemo vam se ubrzo.</div></div>";
+    // if (mail($to, $subject, $message, $headers)) {
+    //     echo  "<div class=\"response--message\"><div>Poruka uspešno poslata!</div><div>Javićemo vam se ubrzo.</div></div>";
+    // } else {
+    //     echo "<div class=\"response--message\"><div> NOT OK </div></div>";
+    // }
+    if(wp_mail($to, $subject, $message, $headers)) {
+        wp_redirect(get_permalink(get_page_by_title('Kontakt')));
     } else {
-        echo "<div class=\"response\"><div> NOT OK </div></div>";
+        echo "ne valja";
     }
 ?>
+</div>
 <?php get_footer(); ?>
